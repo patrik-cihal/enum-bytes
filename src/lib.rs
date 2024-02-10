36bda7bc85331e36a0da4315c400820d67935a1d
Patrik Cihal <patrik.cihal1234@gmail.com>
2024-02-10 20:06:10 +0100

    Extension and path parsing

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@ use syn::{parse_macro_input, Data, DeriveInput, ItemEnum, Lit, Meta, MetaNameVal
 pub fn enum_bytes(args: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let name = input.ident; // The name of the enum
-    let data_enum = if let Data::Enum(data_enum) = input.data {
+    let name = input.ident.clone(); // The name of the enum
+    let data_enum = if let Data::Enum(data_enum) = input.data.clone() {
         data_enum
     } else {
         // Not an enum, so panic or handle as necessary
@@ -31,14 +31,10 @@ pub fn enum_bytes(args: TokenStream, input: TokenStream) -> TokenStream {
     });
 
     let expanded = quote! {
-        impl Into<u32> for #name {
-            fn into(self) -> u32 {
-                self as u32
-            }
-        }
+        #input
 
-        impl Textures for #name {
-            fn bytes(&self) -> Vec<u8> {
+        impl Into<Vec<u8>> for #name {
+            fn into(&self) -> Vec<u8> {
                 match self {
                     #( #variants )*
                 }
